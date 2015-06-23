c9c0ed77d734b48b6597319553250d290874daec
Christian Loza <Christianlzap@gmail.com>
2015-06-23 17:50:10 -0500

    -Agregado

---
+++ src/com/conexion/conexion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.conexion;
+
+/**
+ *
+ * @author Christianlp
+ * @date 23/06/2015
+ * @time 05:49:53 PM
+ * @package com.conexion
+ * @project proyectoCI
+ */
+public class conexion {
+
+}
