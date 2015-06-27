3364b5a1a89e7bf1ccfc7ae725bc64d8a82ccea5
Gustavo Adolfo <Gustavo Adolfo@gustavo>
2015-06-26 19:20:00 -0500

    probando ip

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import com.Gui.LoginForm;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,6 +24,22 @@ public class Main {
          System.out.println("Hola compañeritos");
          System.out.println("Daniel Alcca no entra aun al systema");
          System.out.println("Solucionando");
+         LoginForm jf = new LoginForm();
+         jf.setVisible(true);
+         jf.setLocationRelativeTo(null);
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+ 
          
 
     }
